6c3e7bfe010058903816435acad3edac76f14baf
ralf.philipsen <ralf.philipsen@pttrns.ai>
2021-10-15 13:00:16 +0200

    Add config

--- src/repositories/database.py
+++ src/repositories/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import yaml
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres_fast_api:5432/postgres"
+
+with open(os.path.join("src", "config", "conf.yml"), 'r') as stream:
+    config = yaml.safe_load(stream)
+    SQLALCHEMY_DATABASE_URL = config['SQLALCHEMY_DATABASE_URL']
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
